3d54f05a6c01931ec5c7b6bc8473df378ffbe566
M <mirlovin@gmail.com>
2024-07-16 10:11:29 +0200

    v0  Menu initial commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,35 +36,45 @@
             audioOnlyButton = new RadioButton();
             label1 = new Label();
             label2 = new Label();
+            listResolutions = new ListBox();
+            label3 = new Label();
+            getDetailsButton = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            panel1 = new Panel();
             SuspendLayout();
             // 
             // youtubeURLTextBox
             // 
-            youtubeURLTextBox.Location = new Point(136, 83);
+            youtubeURLTextBox.Location = new Point(120, 27);
             youtubeURLTextBox.Name = "youtubeURLTextBox";
-            youtubeURLTextBox.Size = new Size(459, 23);
+            youtubeURLTextBox.Size = new Size(350, 23);
             youtubeURLTextBox.TabIndex = 0;
             // 
             // downloadButton
             // 
-            downloadButton.Location = new Point(601, 83);
+            downloadButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            downloadButton.Location = new Point(259, 466);
             downloadButton.Name = "downloadButton";
-            downloadButton.Size = new Size(75, 23);
+            downloadButton.Size = new Size(98, 42);
             downloadButton.TabIndex = 1;
-            downloadButton.Text = "Download";
+            downloadButton.Text = "DOWNLOAD";
             downloadButton.UseVisualStyleBackColor = true;
             downloadButton.Click += downloadButton_Click;
             // 
             // txtFolderPath
             // 
-            txtFolderPath.Location = new Point(134, 156);
+            txtFolderPath.Location = new Point(120, 124);
             txtFolderPath.Name = "txtFolderPath";
-            txtFolderPath.Size = new Size(461, 23);
+            txtFolderPath.Size = new Size(350, 23);
             txtFolderPath.TabIndex = 2;
             // 
             // browseButton
             // 
-            browseButton.Location = new Point(601, 157);
+            browseButton.Location = new Point(486, 125);
             browseButton.Name = "browseButton";
             browseButton.Size = new Size(75, 23);
             browseButton.TabIndex = 3;
@@ -75,7 +85,7 @@
             // videoAndAudioButton
             // 
             videoAndAudioButton.AutoSize = true;
-            videoAndAudioButton.Location = new Point(682, 72);
+            videoAndAudioButton.Location = new Point(198, 79);
             videoAndAudioButton.Name = "videoAndAudioButton";
             videoAndAudioButton.Size = new Size(113, 19);
             videoAndAudioButton.TabIndex = 4;
@@ -86,7 +96,7 @@
             // audioOnlyButton
             // 
             audioOnlyButton.AutoSize = true;
-            audioOnlyButton.Location = new Point(682, 97);
+            audioOnlyButton.Location = new Point(346, 79);
             audioOnlyButton.Name = "audioOnlyButton";
             audioOnlyButton.Size = new Size(85, 19);
             audioOnlyButton.TabIndex = 5;
@@ -98,7 +108,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(24, 86);
+            label1.Location = new Point(22, 30);
             label1.Name = "label1";
             label1.Size = new Size(82, 15);
             label1.TabIndex = 6;
@@ -108,17 +118,112 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(24, 159);
+            label2.Location = new Point(22, 130);
             label2.Name = "label2";
             label2.Size = new Size(94, 15);
             label2.TabIndex = 7;
-            label2.Text = "Download path:";
+            label2.Text = "Download Path:";
+            // 
+            // listResolutions
+            // 
+            listResolutions.FormattingEnabled = true;
+            listResolutions.ItemHeight = 15;
+            listResolutions.Location = new Point(437, 176);
+            listResolutions.Name = "listResolutions";
+            listResolutions.Size = new Size(85, 19);
+            listResolutions.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(357, 176);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Resolutions:";
+            // 
+            // getDetailsButton
+            // 
+            getDetailsButton.Location = new Point(486, 28);
+            getDetailsButton.Name = "getDetailsButton";
+            getDetailsButton.Size = new Size(75, 23);
+            getDetailsButton.TabIndex = 10;
+            getDetailsButton.Text = "Get details";
+            getDetailsButton.UseVisualStyleBackColor = true;
+            getDetailsButton.Click += getDetailsButton_Click;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.Black;
+            label4.BorderStyle = BorderStyle.Fixed3D;
+            label4.Location = new Point(45, 64);
+            label4.Name = "label4";
+            label4.Size = new Size(500, 1);
+            label4.TabIndex = 11;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(22, 79);
+            label5.Name = "label5";
+            label5.Size = new Size(95, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Download Type:";
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.Black;
+            label6.BorderStyle = BorderStyle.Fixed3D;
+            label6.Location = new Point(45, 110);
+            label6.Name = "label6";
+            label6.Size = new Size(500, 1);
+            label6.TabIndex = 13;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.BackColor = Color.Black;
+            label7.BorderStyle = BorderStyle.Fixed3D;
+            label7.Location = new Point(42, 166);
+            label7.Name = "label7";
+            label7.Size = new Size(500, 1);
+            label7.TabIndex = 14;
+            label7.Text = "label7";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(259, 176);
+            label8.Name = "label8";
+            label8.Size = new Size(58, 15);
+            label8.TabIndex = 15;
+            label8.Text = "VIDEO(S)";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(45, 213);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(477, 161);
+            panel1.TabIndex = 16;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 315);
+            ClientSize = new Size(584, 561);
+            Controls.Add(panel1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(getDetailsButton);
+            Controls.Add(label3);
+            Controls.Add(listResolutions);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(audioOnlyButton);
@@ -127,7 +232,7 @@
             Controls.Add(txtFolderPath);
             Controls.Add(downloadButton);
             Controls.Add(youtubeURLTextBox);
-            Name = "YoutubeDownloader";
+            Name = "Form1";
             Text = "YoutubeDownloader";
             ResumeLayout(false);
             PerformLayout();
@@ -143,5 +248,14 @@
         private RadioButton audioOnlyButton;
         private Label label1;
         private Label label2;
+        private ListBox listResolutions;
+        private Label label3;
+        private Button getDetailsButton;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Panel panel1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,44 +1,68 @@
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
 namespace YoutubeDownloader
 {
     public partial class Form1 : Form
     {
+        private readonly YoutubeClient ytClient;
+        private string selectedResolution;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ytClient = new YoutubeClient();
+        }
+
         static void downloadAudioVideo(string URL, string downloadPath)
         {
 
+
         }
+
         static void downloadAudioOnly(string URL, string downloadPath)
         {
 
         }
-        public Form1()
-        {
-            InitializeComponent();
-        }
 
-        private void downloadButton_Click(object sender, EventArgs e)
+        private async void getDetailsButton_Click(object sender, EventArgs e)
         {
-            if (txtFolderPath.Text == "")
+            var videoUrl = youtubeURLTextBox.Text;
+            if (string.IsNullOrWhiteSpace(videoUrl))
             {
-                MessageBox.Show("Please select download path before proceeding.", "Download Path Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Please enter the Youtube URL before proceeding.", "Download Path Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-            if (videoAndAudioButton.Checked)
+            try
             {
-                downloadAudioVideo(youtubeURLTextBox.Text, txtFolderPath.Text);
+                var video = await ytClient.Videos.GetAsync(videoUrl);
+                //listResolutions.Text = $"Video Title: {video.Title}";
+                //MessageBox.Show(listResolutions.Text);
 
-                MessageBox.Show("Video download initiated.", "Download", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                var streamManifest = await ytClient.Videos.Streams.GetManifestAsync(video.Id);
+                var streamInfos = streamManifest.GetMuxedStreams();
 
+                listResolutions.Items.Clear();
+                /*foreach (var streamInfo in streamInfos)
+                {
+                    listResolutions.Items.Add($"{streamInfo.VideoQualityLabel} - {streamInfo.Container.Name}");
+                }
+                */
+                if (listResolutions.Items.Count > 0)
+                {
+                    listResolutions.SelectedIndex = 0;
+                }
+                else
+                {
+                    MessageBox.Show("No available qualities found.");
+                }
             }
-            else if (audioOnlyButton.Checked)
+            catch (Exception ex)
             {
-                downloadAudioOnly(youtubeURLTextBox.Text, txtFolderPath.Text);
-                MessageBox.Show("Audio download initiated.", "Download", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("Please select a download option before proceeding.", "Selection Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show($"Error fetching video qualities: {ex.Message}");
             }
         }
+
         private void browseButton_Click(object sender, EventArgs e)
         {
             // Create a new instance of FolderBrowserDialog
@@ -50,6 +74,39 @@ namespace YoutubeDownloader
                     // Get the selected folder path and assign it to the TextBox
                     txtFolderPath.Text = folderBrowserDialog.SelectedPath;
                 }
+            }
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            var videoUrl = youtubeURLTextBox.Text;
+            var downloadPath = txtFolderPath.Text;
+            if (string.IsNullOrWhiteSpace(videoUrl))
+            {
+                MessageBox.Show("Please enter the Youtube URL before proceeding.", "Download Path Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(downloadPath))
+            {
+                MessageBox.Show("Please select download path before proceeding.", "Download Path Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (videoAndAudioButton.Checked)
+            {
+                downloadAudioVideo(videoUrl, downloadPath);
+
+                MessageBox.Show("Video download initiated.", "Download", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else if (audioOnlyButton.Checked)
+            {
+                downloadAudioOnly(videoUrl, downloadPath);
+                MessageBox.Show("Audio download initiated.", "Download", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select a download option before proceeding.", "Selection Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
     }
